af67d344ee980c3333730c0684de12a6c6dd63f5
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-18 12:43:33 -0600

    Created new model validations and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :viewing_partys, dependent: :destroy
   has_many :attendees
   has_many :viewing_partys, through: :attendees
-  
+
   validates_presence_of :user_name, :email
   validates_uniqueness_of :email
+  has_secure_password 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :user_name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+    it { should have_secure_password }
+  end
+
+    it "can test the password attribute " do
+      user = User.create(user_name: "Luke", email: "luke@test.com", password: "test123", password_confirmation: "test123")
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('test123')
   end
 end
